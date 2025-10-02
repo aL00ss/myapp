18ac54395704b47d7d5ae1b8fe9d70e7c4c32def
aL00ss <alexandrebesnier002@gmail.com>
2025-10-02 16:53:52 +0200

    Mise à jour de la page d'accueil pour utiliser un bouton ionique et ajout de la navigation vers la page 2 avec des paramètres.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
-
+import { Router} from '@angular/router';
+import { NavigationExtras } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
+
+
 export class HomePage {
+  
 Montexte="Bonjour";
-  constructor() {}
-  MonClick(){
-    alert("Vous avez cliqué");
+  constructor(private router: Router, private activeRoute : ActivatedRoute) {
+    
   }
+  MonClick(){
+    this.router.navigate(['/page2', navigationExtras]);
+  }
+  
 
 }
+  // Pour passer des paramètres
+let navigationExtras: NavigationExtras = {
+state : {
+param1: " valeur "
+}
+};
--- src/app/page2/page2.page.ts
+++ src/app/page2/page2.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-page2',
   templateUrl: './page2.page.html',
   styleUrls: ['./page2.page.scss'],
+  standalone: false,
 })
 export class Page2Page implements OnInit {
 
